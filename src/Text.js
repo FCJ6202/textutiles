c38af1f766af56de7af51efb7de85a61234b60ef
FCJ <iit2020218@iiita.ac.in>
2021-12-11 01:19:42 +0530

    Navbar complete

---
+++ src/Text.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar 
+         title = "textutiles"
+         about = "About"
+         home = "Home"
+      />
+    </>
+  );
+}
+
+export default App;
